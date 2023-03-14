a57b5c4f8a6c79da4bae1de6a15964e684792d25
Eskandar Hany Ayad Boshra Ayad <hanyayad71@gmail.com>
2023-03-14 22:14:28 +0100

    Updated user controlls to triger events when interacting with new game objects

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerController : MonoBehaviour
 {
@@ -8,11 +9,12 @@ public class playerController : MonoBehaviour
     private float turnSpeed=70.0f;
     private float horizontalInput ;
     private float forwardInput ;
-
+    public int counter=0;
+    public int maxCoins;
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxCoins=GameObject.FindGameObjectsWithTag("Coin").Length;
     }
 
     // Update is called once per frame
@@ -25,4 +27,41 @@ public class playerController : MonoBehaviour
         //Rotates the vehicle to left and right
         transform.Rotate(Vector3.up,turnSpeed*horizontalInput*Time.deltaTime);
     }
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Coin"))
+        {
+        //Destroy(other.gameObject);
+        other.gameObject.SetActive(false);
+        Debug.Log("Collision happened");
+        counter++;
+        Debug.Log("Coin Collected"+counter);
+        if(counter==maxCoins){
+            Debug.Log("Won");
+            GetComponent<playerController>().enabled=false;
+        }
+        }
+        if(other.gameObject.CompareTag("Slow"))
+        {
+        //Destroy(other.gameObject);
+        other.gameObject.SetActive(false);
+        Debug.Log("Collision happened");
+        speed=10.0f;
+        }
+        if(other.gameObject.CompareTag("Fast"))
+        {
+        //Destroy(other.gameObject);
+        other.gameObject.SetActive(false);
+        Debug.Log("Collision happened");
+        speed=60.0f;
+        }
+         if(other.gameObject.CompareTag("Lost"))
+            {Debug.Log("Lost The Game");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+}
+
+        
+        }
+       
+        
+
 }
